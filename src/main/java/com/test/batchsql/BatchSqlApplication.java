d78867b33d7afb823f1574e79660024b873f65e1
Max Ivanov <mamalenaralalend@gmail.com>
2022-12-03 18:12:51 +0300

    инициализация проекта

---
+++ src/main/java/com/test/batchsql/BatchSqlApplication.java
@@ -0,0 +1,13 @@
+package com.test.batchsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/batchsql/BatchSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.batchsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
