3dd1384eac39a07caa9175f016e8b19b631f6848
Max Ivanov <mamalenaralalend@gmail.com>
2022-12-09 20:07:46 +0300

    замена конфига БД

--- src/main/java/com/test/batchsql/config/ConfigDbCommon.java
+++ src/main/java/com/test/batchsql/config/ConfigDbCommon.java
@@ -1,41 +1,31 @@
 package com.test.batchsql.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
 
 import javax.sql.DataSource;
 
 /**
- * Устанавливает конфигурации и соединение с БД
- */
+ * Подготавливает высокоуровневую обвязку по имеющимся в энвайронменте датасурсам
+*/
 @Configuration
-public class ConfigDbCommon extends DefaultDbConfig{
-    @Bean
-    @Qualifier("postgres")
-    @ConfigurationProperties(prefix = "app.db.postgres")
-    SpringDataJdbcProperties frapPgJdbcProperties() {
-        return new SpringDataJdbcProperties();
-    }
-
-    @Bean
-    @Qualifier("postgres")
-    public DataSource frapPgDataSource(@Qualifier("postgres") SpringDataJdbcProperties properties) {
-        return hikariDataSource("POSTGRES", properties);
-    }
-
-    @Bean
-    @Qualifier("postgres")
-    JdbcTemplate frapPgJdbcTemplate(@Qualifier("postgres") DataSource dataSource) {
+public class ConfigDbCommon {
+    @Bean("jdbcTemplate")
+    JdbcTemplate jdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
         return new JdbcTemplate(dataSource);
     }
 
-    @Bean
-    @Qualifier("postgres")
-    NamedParameterJdbcTemplate namedFrapJdbcTemplate(@Qualifier("postgres") DataSource dataSource) {
+    @Bean("namedParameterJdbcTemplate")
+    NamedParameterJdbcTemplate namedFsrarOpenDataJdbcTemplate(@Qualifier("dataSource") DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
+    }
+    @Bean("txManager")
+    TransactionManager txManager(@Qualifier("dataSource") DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 }
