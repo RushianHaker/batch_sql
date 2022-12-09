9ca9eec22bc9e3ef269009867b13f1eb9c225d41
Max Ivanov <mamalenaralalend@gmail.com>
2022-12-09 20:08:47 +0300

    добавил конфиг в тесты, для лайнчинга и запуска тестовой БД

---
+++ src/test/resources/test-scheme/ddl.sql
@@ -0,0 +1,5 @@
+CREATE  TABLE cards (
+	id                   bigint  NOT NULL  ,
+    name                 varchar(128)      ,
+    full_name            varchar(256)
+ );
---
+++ src/test/resources/test-scheme/dml-generated.sql
@@ -0,0 +1,5 @@
+INSERT INTO cards( id, name, full_name) VALUES ( 0, 'Mark', 'Vasquez');
+INSERT INTO cards( id, name, full_name) VALUES ( 0, 'Anna', 'Ivanov');
+INSERT INTO cards( id, name, full_name) VALUES ( 0, 'Cris', 'Dolan');
+INSERT INTO cards( id, name, full_name) VALUES ( 0, 'Devid', 'Isaev');
+INSERT INTO cards( id, name, full_name) VALUES ( 0, 'Andry', 'Markovich');
